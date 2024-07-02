5422f86a95b52d72f6f8c2e003af1b8c73426c74
Shele_jc66@student.itstep.org <Shele_jc66@student.itstep.org>
2024-07-02 15:36:53 +0300

    02_Array

--- 02_Array/Program.cs
+++ 02_Array/Program.cs
@@ -1,128 +1,9 @@
-﻿namespace _0_2Array
+﻿namespace _02_Array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region One dimention arr
-            //int[] arr = new int[5];
-            //arr[0] = 10;
-            //arr[1] = 20;
-            //arr[2] = 30;
-            //arr[3] = 40;
-            //arr[4] = 50;
-
-            //Console.WriteLine(arr[0]);
-            //Console.WriteLine(arr[1]);
-            //Console.WriteLine(arr[2]);
-            //Console.WriteLine(arr[3]);
-            //Console.WriteLine(arr[4]);
-
-
-            //int[] arr2 = new int[5];
-            //for (int i = 0; i < arr2.Length; i++)
-            //{
-            //    arr2[i] = i * 2;
-            //}
-
-            //for (int i = 0; i < arr2.Length; i++)
-            //{
-            //    Console.Write(arr2[i] + " ");
-            //}
-            //Console.WriteLine();
-
-
-            //int[] arr3 = new int[5] { 1, 2, 3, 4, 5 };
-            //for (int i = 0; i < arr3.Length; i++)
-            //{
-            //    Console.Write(arr3[i] + " ");
-            //}
-            //Console.WriteLine();
-
-            //int[] arr4 = new int[] { 1, 2, 3, 4, 5, 32, 43, 53, 54, };
-            //for (int i = 0; i < arr4.Length; i++)
-            //{
-            //    Console.Write(arr4[i] + " ");
-            //}
-            //Console.WriteLine();
-
-            //int[] arr5 = new int[10];
-            //for (int i = 0; i < arr5.Length; i++)
-            //{
-            //    Console.Write(arr5[i] + " ");
-            //}
-            //Console.WriteLine();
-
-            //arr5.SetValue(77, 2);
-            //arr5.SetValue(22, 3);
-
-            //foreach (var item in arr4)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            #endregion
-            #region Two dimention arr
-            //int[,] arr = new int[3, 3];
-            //arr[0, 0] = 1;
-            //arr[0, 1] = 2;
-            //arr[0, 2] = 3;
-
-            //arr[1, 0] = 4;
-            //arr[1, 1] = 5;
-            //arr[1, 2] = 6;
-
-            //arr[2, 0] = 7;
-            //arr[2, 1] = 8;
-            //arr[2, 2] = 9;
-            //Console.Write(arr[0, 0] + " ");
-            //Console.Write(arr[0, 1] + " ");
-            //Console.Write(arr[0, 2] + " ");
-            //Console.WriteLine();
-            //Console.Write(arr[1, 0] + " ");
-            //Console.Write(arr[1, 1] + " ");
-            //Console.Write(arr[1, 2] + " ");
-            //Console.WriteLine();
-            //Console.Write(arr[2, 0] + " ");
-            //Console.Write(arr[2, 1] + " ");
-            //Console.Write(arr[2, 2] + " ");
-            //Console.WriteLine();
-            ////2
-            //int[,] arr2 = new int[3, 5];
-            //Console.WriteLine(arr2.Length);
-            //for (int i = 0; i < arr2.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < arr2.GetLength(1); j++)
-            //    {
-            //        arr2[i, j] = i * j + 1;
-            //    }
-            //}
-            //for (int i = 0; i <= arr2.GetUpperBound(0); i++)
-            //{
-            //    for (int j = 0; j <= arr2.GetUpperBound(1); j++)
-            //    {
-            //        Console.Write( arr2[i, j] = i * j + 1);
-            //    }
-            //    Console.WriteLine();
-            //}
-            /////4
-            //int[,] arr4 =
-            //{
-            //      { 1,2,3},
-            //      {4,5,6 },
-            //      {7,8,9 }
-            //  };
-            //Console.WriteLine(arr4.Length);
-            //Console.WriteLine(arr4);
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 3; j++)
-            //    {
-            //        Console.Write($"{arr4[i, j]} ");
-            //    }
-            //    Console.WriteLine();
-            //}
-            #endregion
-            #region DZ
             Console.WriteLine("Zavdanna 1");
             int[] arr = { 2, 3, 5, 3, 7, 5 };
             int evenNumber = 0;
@@ -141,12 +22,18 @@
             }
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < i; j++)
+                bool isUnique = true;
+                for (int j = 0; j < arr.Length; j++)
                 {
-                    if (arr[i] != arr[j])
+                    if (i != j && arr[i] == arr[j])
                     {
-                        uniqueNumber++;
+                        isUnique = false;
+                        break;
                     }
+                }
+                if (isUnique)
+                {
+                    uniqueNumber++;
                 }
             }
             Console.WriteLine("Even number: " + evenNumber);
@@ -154,19 +41,191 @@
             Console.WriteLine("Unique number: " + uniqueNumber);
 
             Console.WriteLine("Zavdanna 2");
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Enter the threshold value : ");
 
+            int threshold = int.Parse(Console.ReadLine());
 
+            int LessThanThreshold = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] < threshold)
+                {
+                    LessThanThreshold++;
+                }
+            }
+            Console.WriteLine("Numbers less than threshold: " + LessThanThreshold);
 
+            Console.WriteLine("Zavdanna 3");
+            int[] A = new int[5];
+            double[,] B = new double[3, 4];
+            Random random = new Random();
 
+            Console.WriteLine("Enter 5 integers for array A:");
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
 
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    B[i, j] = random.NextDouble() * 100;
+                }
+            }
 
+            Console.WriteLine("Array A:");
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
 
+            Console.WriteLine("Array B:");
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    Console.Write($"{B[i, j]:F2} ");
+                }
+                Console.WriteLine();
+            }
 
+            double maxElement = double.MinValue;
+            double minElement = double.MaxValue;
+            double totalSum = 0;
+            double totalProduct = 1;
 
+            foreach (int value in A)
+            {
+                if (value > maxElement) maxElement = value;
+                if (value < minElement) minElement = value;
+                totalSum += value;
+                totalProduct *= value;
+            }
 
+            foreach (double value in B)
+            {
+                if (value > maxElement) maxElement = value;
+                if (value < minElement) minElement = value;
+                totalSum += value;
+                totalProduct *= value;
+            }
 
+            int sumEvenA = 0;
+            foreach (int value in A)
+            {
+                if (value % 2 == 0)
+                {
+                    sumEvenA += value;
+                }
+            }
 
-            #endregion
+            double sumOddColumnsB = 0;
+            for (int j = 1; j < B.GetLength(1); j += 2)
+            {
+                for (int i = 0; i < B.GetLength(0); i++)
+                {
+                    sumOddColumnsB += B[i, j];
+                }
+            }
+
+            Console.WriteLine("Max element: " + maxElement);
+            Console.WriteLine("Min element: " + minElement);
+            Console.WriteLine("Total sum: " + totalSum);
+            Console.WriteLine("Total product: " + totalProduct);
+            Console.WriteLine("Sum of even elements in A: " + sumEvenA);
+            Console.WriteLine("Sum of odd columns in B: " + sumOddColumnsB);
+
+            Console.WriteLine("Zavdanna 4");
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(-100, 101);
+                }
+            }
+            Console.WriteLine("2D Array:");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j],5} ");
+                }
+                Console.WriteLine();
+            }
+            int minElem = array[0, 0];
+            int maxElem = array[0, 0];
+            int minIndexI = 0, minIndexJ = 0;
+            int maxIndexI = 0, maxIndexJ = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < minElem)
+                    {
+                        minElem = array[i, j];
+                        minIndexI = i;
+                        minIndexJ = j;
+                    }
+                    if (array[i, j] > maxElem)
+                    {
+                        maxElem = array[i, j];
+                        maxIndexI = i;
+                        maxIndexJ = j;
+                    }
+                }
+            }
+            int sumBetween = 0;
+            bool counting = false;
+
+            if ((minIndexI < maxIndexI) || (minIndexI == maxIndexI && minIndexJ < maxIndexJ))
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (i == minIndexI && j == minIndexJ)
+                        {
+                            counting = true;
+                        }
+                        if (counting)
+                        {
+                            sumBetween += array[i, j];
+                        }
+                        if (i == maxIndexI && j == maxIndexJ)
+                        {
+                            counting = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (i == maxIndexI && j == maxIndexJ)
+                        {
+                            counting = true;
+                        }
+                        if (counting)
+                        {
+                            sumBetween += array[i, j];
+                        }
+                        if (i == minIndexI && j == minIndexJ)
+                        {
+                            counting = false;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Minimum element: {minElem} (indices [{minIndexI}, {minIndexJ}])");
+            Console.WriteLine($"Maximum element: {maxElem} (indices [{maxIndexI}, {maxIndexJ}])");
+            Console.WriteLine($"Sum of elements between minimum and maximum: {sumBetween}");
         }
     }
 }
