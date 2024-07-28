6d596bba61465ec6bb82ea968e0699baad92527e
Shele_jc66@student.itstep.org <Shele_jc66@student.itstep.org>
2024-07-28 21:27:34 +0300

    Dictionary

--- 13_Dictionary/Program.cs
+++ 13_Dictionary/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _13_Dictionary
+﻿using System.Text;
+
+namespace _13_Dictionary
 {
     public class PhoneBook
     {
@@ -58,8 +60,28 @@
             ph.RemoveByKey("0924446666");
             ph.Print();
             Console.WriteLine();
-
-
+            Console.WriteLine("Zavdanna 2");
+            Console.OutputEncoding = Encoding.UTF8;//щоб виводило українські букви    PS:для вчительки
+            string text = "Ось будинок, який збудував Джек. А це пшениця, яка у темній коморі зберігається у будинку, який збудував Джек. А це веселий птах-синиця, який часто краде пшеницю, яка в темній коморі зберігається у будинку, який збудував Джек.";
+            string[] words = text.Split(new char[] { ' ', ',', '.', '!', '?', '-', '—', ':', ';', '(', ')' },StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (wordCounts.ContainsKey(word))
+                {
+                    wordCounts[word]++;
+                }
+                else
+                {
+                    wordCounts[word] = 1;
+                }
+            }
+            Console.WriteLine("Слово \t\tКількість");
+            Console.WriteLine("-------------------------");
+            foreach (var item in wordCounts)
+            {
+                Console.WriteLine("{0,-15} {1,-5}", item.Key, item.Value);
+            }
         }
     }
 }
